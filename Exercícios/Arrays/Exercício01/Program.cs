869de5e6185cf0e051edf40e8a0fa95c81430251
FIC <fic@info.local>
2025-03-22 15:15:33 -0300

    push Exercício01 - Arrays

---
+++ Exercícios/Arrays/Exercício01/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1. * *Soma dos Elementos**
+//    - Crie um programa que receba 5 números inteiros, armazene-os em um array e exiba a soma total dos elementos.
+
+int[] numeros = new int[5];
+int soma = 0;
+
+for(int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"Digite o {i+1} número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    soma += numeros[i];
+}
+
+Console.WriteLine($"SOMA = {soma}");
