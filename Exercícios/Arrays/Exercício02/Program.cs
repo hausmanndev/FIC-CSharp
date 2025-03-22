3dcaaf6aa1aa892350ef9a76d5e79eaf35c79aef
FIC <fic@info.local>
2025-03-22 15:53:32 -0300

    push Exercício02 e 03 - Arrays

---
+++ Exercícios/Arrays/Exercício02/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2. * *Maior e Menor Valor**
+//    - Peça ao usuário 10 números e armazene-os em um array. Em seguida, exiba o maior e o menor valor inserido.
+
+int[] nums = new int[10];
+
+for(int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"Digite o {i + 1} número: ");
+    nums[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"O maior número do array é {Enumerable.Max(nums)}");
+Console.WriteLine($"O menor número do array é {Enumerable.Min(nums)}");
+//Console.WriteLine($"O menor número do array é {nums[0]}");
---
+++ Exercícios/Arrays/Exercício03/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//3. * *Média dos Valores**
+//    - Receba 6 números inteiros e armazene-os em um array. Depois, calcule e exiba a média dos valores.
+
+
+
+int[] nums = new int[6];
+double soma = 0;
+
+for (int i =0; i < nums.Length; i++)
+{
+    Console.WriteLine($"Digite o {i} número");
+    nums[i] = int.Parse(Console.ReadLine());
+
+    soma += nums[i];
+}
+
+double media = soma / nums.Length;
+Console.WriteLine($"MEDIA = {media}");
