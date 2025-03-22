b1c2a63af1d243c75a64b32295881f7467fc9e4d
FIC <fic@info.local>
2025-03-22 16:32:20 -0300

    push Exercício04 - Arrays

---
+++ Exercícios/Arrays/Exercício04/Program.cs
@@ -0,0 +1,30 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//1. * *Contagem de Números Pares**
+//    - Peça ao usuário 10 números, armazene em um array e exiba a quantidade de números pares inseridos.
+
+int[] nums = new int[10];
+int[] pares = new int[10];
+
+for(int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine($"Digite o {i} numero: ");
+    nums[i] = int.Parse(Console.ReadLine());
+
+    if (nums[i] % 2 == 0)
+    {
+        pares[i] = nums[i];
+    }
+}
+
+Console.WriteLine("Números Pares:");
+int cont = 0;
+for(int i = 0; i < pares.Length; i++)
+{
+    if (pares[i] > 0)
+    {
+        Console.WriteLine(pares[i]);
+        cont++;
+    }
+}
+
+Console.WriteLine($"A quantidade de números pares é {cont}");
