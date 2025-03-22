7651e9181a3fd415cced10b3a07ab94d3827d538
FIC <fic@info.local>
2025-03-22 16:53:55 -0300

    push Exercício05 - Arrays

---
+++ Exercícios/Arrays/Exercício05/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2. * *Inversão de Elementos**
+//    - Crie um programa que recebe 8 números em um array e exibe os valores na ordem inversa à que foram inseridos.
+
+int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+for (int i = nums.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(nums[i]);
+}
