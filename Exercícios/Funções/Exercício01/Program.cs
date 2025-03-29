47f092b773687e2326ddc7a4f93f375e6892791b
FIC <fic@info.local>
2025-03-29 10:28:53 -0300

    push Exercício01 - Funções

---
+++ Exercícios/Funções/Exercício01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1. * *Função de Soma**
+//    - Crie uma função que recebe dois números e retorna a soma deles.
+
+static double Somar()
+{
+    List<double> nums = new List<double>();
+
+    for(int i = 0; i < 3; i++)
+    {
+        Console.Write($"Digite o número {i+1}: ");
+        nums.Add(Convert.ToDouble(Console.ReadLine()));
+    }
+
+    double soma = nums.Sum() / nums.Count();
+    return soma;
+}
+
+Console.WriteLine(Somar());
