81dc83969bebb6c0a45cae07374ddd58167a9f14
FIC <fic@info.local>
2025-03-29 11:21:06 -0300

    push Exercício04 - Funções

---
+++ Exercícios/Funções/Exercício04/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1. * *Função de Fatorial**
+//    - Implemente uma função que recebe um número e retorna seu fatorial.
+
+Console.Write("Digite um número: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+static int Fatorar(int n)
+{
+    int fatorial = 1;
+    while(n > 1)
+    {
+        fatorial *= n;
+        n--;
+    }
+
+    return fatorial;
+}
+
+Console.WriteLine(Fatorar(n));
