6093d15871c5566d82f5f047247d75622c618176
FIC <fic@info.local>
2025-03-29 11:43:57 -0300

    push Exercício05 - Funções

---
+++ Exercícios/Funções/Exercício05/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//2. * *Função para Inverter String**
+//    - Faça uma função que recebe uma string e retorna a string invertida.
+
+Console.Write("Digite uma palavra / frase: ");
+string s = Console.ReadLine();
+
+static string InverterString(string s)
+{
+    int cont = 0;
+    string sInvertido = new string(s.Reverse().ToArray());
+
+    return sInvertido;
+
+}
+
+Console.WriteLine(InverterString(s));
