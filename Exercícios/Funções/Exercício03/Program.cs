986d098218e085abbe95b6f32c1a1c55965329e1
FIC <fic@info.local>
2025-03-29 10:54:11 -0300

    push Exercício03 - Funções

---
+++ Exercícios/Funções/Exercício03/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//3. * *Função de Par ou Ímpar**
+//    - Escreva uma função que recebe um número e retorna "Par" ou "Ímpar".
+
+Console.Write("Digite um número: ");
+int n = int.Parse(Console.ReadLine());
+
+static void ParOuImpar(int n)
+{
+    if(n % 2 == 0)
+    {
+        Console.WriteLine($"O número {n} é PAR!");
+    } else
+    {
+        Console.WriteLine($"O número {n} é ÍMPAR!");
+    }
+}
+
+ParOuImpar(n);
