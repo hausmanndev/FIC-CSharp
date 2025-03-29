6ad561c055294583f91e1637f2282c6519f23b0a
FIC <fic@info.local>
2025-03-29 10:06:36 -0300

    push Aula03

---
+++ Aula03/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// FUNÇÕES
+
+static double Somar(double n1, double n2)
+{
+    return n1 + n2;
+}
+
+static void MostrarSoma()
+{
+    Console.Write("Digite o primeiro número: ");
+    double n1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Digite o segundo número: ");
+    double n2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine($"A soma entre os dois números é igual a {Somar(n1, n2)}");
+}
+
+MostrarSoma();
---
+++ Exercícios/Arrays/Exercício07/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1. * *Remover Duplicatas * *
+//    -Receba 10 números em um array e exiba-os sem valores duplicados.
+
+int[] valores = { 2, 5, 7, 2, 8, 9, 4, 5, 6, 10 };
+List<int> duplicados = new List<int>();
+
+for(int i = 0; i < valores.Length; i++)
+{
+    for(int j = i+1; j < valores.Length; j++)
+    {
+        if (valores[i] == valores[j])
+        {
+            duplicados.Add(valores[i]);
+        }
+    }
+}
