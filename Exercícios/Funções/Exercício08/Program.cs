02b643f6b630b9f139f66b910749efd1dfa8bd73
FIC <fic@info.local>
2025-03-29 15:04:51 -0300

    push Exercício08 - Funções

---
+++ Exercícios/Funções/Exercício08/Program.cs
@@ -0,0 +1,27 @@
+﻿//2. * *Função de Busca em Array**
+//    - Faça uma função que recebe um array e um número, retornando `true`
+//    se o número estiver no array e `false` caso contrário.
+int[] array = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Digite o número {i+1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Digite um número: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+static bool VerificaArray(int[] array, int n)
+{
+    foreach(int i in array)
+    {
+        if(n == i)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Console.WriteLine(VerificaArray(array, n));
