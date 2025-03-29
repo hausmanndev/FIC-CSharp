d0f63e990c99c6032a581a6ccc402b7d0b1dab82
FIC <fic@info.local>
2025-03-29 15:20:25 -0300

    push Exercício09 - Funções

---
+++ Exercícios/Funções/Exercício09/Program.cs
@@ -0,0 +1,11 @@
+﻿//3. * *Função Recursiva para Fibonacci**
+//    - Implemente uma função recursiva que recebe um número `n` e retorna o `n`ésimo termo da sequência de Fibonacci.
+
+Console.Write("Digite um número: ");
+int n = int.Parse(Console.ReadLine());
+
+static int Fibonacci(int n)
+{
+
+    return Fibonacci(n);
+}
