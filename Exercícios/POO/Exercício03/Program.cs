a82ee2773ecaf9ebec0c19844502d1c080812097
FIC <fic@info.local>
2025-04-12 15:57:29 -0300

    push Exercício03 - POO

---
+++ Exercícios/POO/Exercício03/Program.cs
@@ -0,0 +1,30 @@
+﻿//3. * *Encapsulamento - Propriedades * *
+//    -Modifique a classe `Pessoa` para que `Idade` só possa ser alterada se o valor for maior que zero.
+
+class Pessoa
+{
+    int idade = 22;
+
+
+    public void AlteraIdade()
+    {
+        Console.Write("Digite a nova idade: ");
+        int novaIdade = int.Parse(Console.ReadLine());
+
+        if (novaIdade > 0)
+        {
+            idade = novaIdade;
+            Console.WriteLine($"A nova idade é {idade}.");
+        }
+        else
+        {
+            Console.WriteLine($"Idade inalterada. Idade: {idade}");
+        }
+    }
+
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.AlteraIdade();
+    }
+}
