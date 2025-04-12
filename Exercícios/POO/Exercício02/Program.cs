d4a3736fdd232df1f883d86c396f2fc5e3151c3a
FIC <fic@info.local>
2025-04-12 15:43:06 -0300

    push Exercício02 - POO

---
+++ Exercícios/POO/Exercício02/Program.cs
@@ -0,0 +1,20 @@
+﻿//2. * *Métodos Simples * *
+//    -Adicione à classe `Pessoa` um método `ExibirDados()` que imprime o nome e a idade no console.
+
+class Pessoa
+{
+    string nome = "Gabriel";
+    int idade = 22;
+
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Nome: {nome} | Idade: {idade}");
+    }
+
+    public static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.ExibirDados();
+    }
+}
