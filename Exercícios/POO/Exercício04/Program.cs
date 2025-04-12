4c1af6143072e2b691822d1bb06525840ad3b54a
FIC <fic@info.local>
2025-04-12 16:42:12 -0300

    push Exercício04 - POO

---
+++ Exercícios/POO/Exercício04/Program.cs
@@ -0,0 +1,23 @@
+﻿//1. * *Construtores * *
+//    -Adicione um construtor à classe `Pessoa` que recebe `Nome` e `Idade` como parâmetros ao instanciar um objeto.
+
+class Pessoa
+{
+    string Nome;
+    int Idade;
+    public Pessoa(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+
+    public static void Main()
+    {
+        Console.Write("Digite o nome: ");
+        string nome = Console.ReadLine();
+        Console.Write("Digte a idade: ");
+        int idade = int.Parse(Console.ReadLine());
+        Pessoa p1 = new Pessoa(nome, idade);
+        Console.WriteLine($"Nome: {p1.Nome} | Idade: {p1.Idade}");
+    }
+}
